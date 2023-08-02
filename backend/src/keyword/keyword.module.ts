5e70d5aa5bcf10f978cc953cfdb978309afacc67
swornim-dh <swornim@dhoon.co>
2023-08-02 18:38:05 +0545

    Keyword set and edit done

---
+++ backend/src/keyword/keyword.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { KeywordService } from './keyword.service';
+import { KeywordController } from './keyword.controller';
+import { Keyword } from './entities/keyword.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [ TypeOrmModule.forFeature([ Keyword ]) ],
+  providers: [ KeywordService ],
+  controllers: [ KeywordController ]
+})
+export class KeywordModule { }
