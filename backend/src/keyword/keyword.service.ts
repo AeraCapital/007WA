f25d64d69acd3fe21f804742ea5543c8ec41123c
swornim-dh <swornim@dhoon.co>
2023-08-04 11:59:22 +0545

    List Keyword Added

--- backend/src/keyword/keyword.service.ts
+++ backend/src/keyword/keyword.service.ts
@@ -10,21 +10,29 @@ import { SetKeywordDto } from './dto/setKeyword.dto';
 export class KeywordService {
     constructor (
         @InjectRepository(Keyword)
-        private keywordReplyRepository: Repository<Keyword>,
+        private keywordRepository: Repository<Keyword>,
     ) { }
 
     async setKeyword (setKeywordDto: SetKeywordDto) {
-        const keywordReply = this.keywordReplyRepository.create(setKeywordDto);
-        return await this.keywordReplyRepository.save(keywordReply);
+        const keywordReply = this.keywordRepository.create(setKeywordDto);
+        return await this.keywordRepository.save(keywordReply);
     }
 
     async updateKeyword (id: string, setKeywordDto: SetKeywordDto) {
-        const keywordReply = await this.keywordReplyRepository.findOneBy({ id: id });
+        const keywordReply = await this.keywordRepository.findOneBy({ id: id });
         if (!keywordReply) {
             throw new NotFoundException(`Keyword with ID ${ id } not found`);
         }
 
-        const updatedKeywordReply = this.keywordReplyRepository.merge(keywordReply, setKeywordDto);
-        return this.keywordReplyRepository.save(updatedKeywordReply);
+        const updatedKeywordReply = this.keywordRepository.merge(keywordReply, setKeywordDto);
+        return this.keywordRepository.save(updatedKeywordReply);
+    }
+
+    async findAll (): Promise<Keyword[]> {
+        return this.keywordRepository.find({});
+    }
+
+    async getReply (keyword: string) {
+        return ((await this.keywordRepository.findOneBy({ keyword: keyword })).reply);
     }
 }
