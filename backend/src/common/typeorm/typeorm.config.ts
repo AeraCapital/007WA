f590a732e054c1e7817415195ab1ff0743c8b55a
swornim-dh <swornim@dhoon.co>
2023-08-05 20:38:19 +0545

    Database Port changed

--- backend/src/common/typeorm/typeorm.config.ts
+++ backend/src/common/typeorm/typeorm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'sassy-db',
-    port: 6432,
+    port: 5432,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
