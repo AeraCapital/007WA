bb0e7dec0858d379b21f7def27c6701607226413
swornim-dh <swornim@dhoon.co>
2023-08-07 11:34:50 +0545

    keyword delete + exception handling

--- backend/src/keyword/keyword.controller.ts
+++ backend/src/keyword/keyword.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, UseGuards, ValidationPipe, UsePipes, Put, Param, Get } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, ValidationPipe, UsePipes, Put, Param, Get, Delete, NotFoundException, HttpException, ConflictException } from '@nestjs/common';
 import { KeywordService } from './keyword.service';
 import { SetKeywordDto } from './dto/setKeyword.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { HTTP_STATUS } from 'src/common/constants/status';
 
 
 @Controller('keyword')
@@ -10,26 +11,69 @@ export class KeywordController {
 
     @Post('set')
     @UsePipes(new ValidationPipe({ transform: true }))
-    setKeyword (@Body() setKeywordDto: SetKeywordDto) {
-        console.log(setKeywordDto);
-        return this.keywordReplyService.setKeyword(setKeywordDto);
+    async setKeyword (@Body() setKeywordDto: SetKeywordDto) {
+        try {
+            const data = await this.keywordReplyService.setKeyword(setKeywordDto);
+            return { statusCode: HTTP_STATUS.CREATED, data };
+
+        } catch (err) {
+
+            if (err instanceof ConflictException) {
+                throw new ConflictException({ statusCode: err.getStatus(), message: err.message });
+
+            }
+
+            throw new HttpException({
+                statusCode: HTTP_STATUS.INTERNAL_SERVER_ERROR,
+                message: 'Unexpected error', error: err.response
+
+            }, HTTP_STATUS.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Put(':id')
     @UsePipes(new ValidationPipe({ transform: true }))
-    updateKeyword (@Param('id') id: string, @Body() setKeywordDto: SetKeywordDto) {
-        return this.keywordReplyService.updateKeyword(id, setKeywordDto);
+    async updateKeyword (@Param('id') id: string, @Body() setKeywordDto: SetKeywordDto) {
+        try {
+            const data = await this.keywordReplyService.updateKeyword(id, setKeywordDto);
+            return { statusCode: HTTP_STATUS.OK, data, message:"Keyword Updated!" };
+        } catch (err) {
+
+            if (err instanceof ConflictException) {
+                throw new NotFoundException({ statusCode: err.getStatus(), message: err.message });
+
+            }
+
+            if (err instanceof NotFoundException) {
+                throw new NotFoundException({ statusCode: HTTP_STATUS.NOT_FOUND, message: err.message });
+            }
+
+            throw new HttpException({ statusCode: HTTP_STATUS.INTERNAL_SERVER_ERROR, message: 'Unexpected error!' }, HTTP_STATUS.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Get()
     @UsePipes(new ValidationPipe({ transform: true }))
-    getAll () {
-        return this.keywordReplyService.findAll();
+    async getAll () {
+        try {
+            const data = await this.keywordReplyService.findAll();
+            return { statusCode: HTTP_STATUS.OK, data };
+        } catch (err) {
+            throw new HttpException({ statusCode: HTTP_STATUS.INTERNAL_SERVER_ERROR, message: 'Unexpected error' }, HTTP_STATUS.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    //   @Post('/get-reply')
-    //   getReply(@Body() setMessageDto: SetMessageDto) {
-    //     const reply = this.keywordReplyService.getMessage(setMessageDto);
-    //     return { reply };
-    //   }
+
+    @Delete(':id')
+    async deleteKeyword (@Param('id') id: number) {
+        try {
+            await this.keywordReplyService.delete(id);
+            return { statusCode: HTTP_STATUS.OK, message: "Successfully deleted" };
+        } catch (err) {
+            if (err instanceof NotFoundException) {
+                throw new NotFoundException({ statusCode: HTTP_STATUS.NOT_FOUND, message: err.message });
+            }
+            throw new HttpException({ statusCode: HTTP_STATUS.INTERNAL_SERVER_ERROR, message: 'Unexpected error', error: err }, HTTP_STATUS.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
