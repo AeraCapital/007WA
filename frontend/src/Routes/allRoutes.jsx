71b3ed23ea09874d830f035f8d52dec2918902c1
unknown <prajwolpakka@gmail.com>
2023-08-13 15:11:41 +0545

    removed unnecessary routes

--- frontend/src/Routes/allRoutes.jsx
+++ frontend/src/Routes/allRoutes.jsx
@@ -1,6 +1,5 @@
 // import EcommerenceProducts from "src/pages/Ecommerence/EcommerenceProducts";
 import { Navigate } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
 
 //Authentication pages
 // import Login1 from "../pages/AuthenticationInner/Login";
@@ -19,28 +18,24 @@ import PagesMaintenance from "../pages/Utility/pages-maintenance";
 import PagesStarter from "../pages/Utility/pages-starter";
 
 // Auth
+import Agents from "pages/Agents";
+import ChangePassword from "pages/Authentication/change-password";
 import ForgotPassword from "pages/Authentication/forgot-password";
-import ResetPassword from "pages/Authentication/reset-password";
-import ResetPasswordSuccess from "pages/Authentication/reset-password-success";
+import Login from "pages/Authentication/login";
 import Logout from "pages/Authentication/logout";
 import SignUp from "pages/Authentication/register";
-import Login from "pages/Authentication/login";
+import ResetPassword from "pages/Authentication/reset-password";
+import ResetPasswordSuccess from "pages/Authentication/reset-password-success";
 import UserProfile from "pages/Authentication/user-profile";
-import ChangePassword from "pages/Authentication/change-password";
-import Keywords from "pages/Keywords";
-import Agents from "pages/Agents";
 import Playground from "pages/AuthenticationInner/page-confirm-mail";
-import Whatsapp from "pages/Whatsapp";
 import Chat from "pages/Chat";
+import Keywords from "pages/Keywords";
 
 const authProtectedRoutes = [
-  { path: "/dashboard", component: <Dashboard /> },
   { path: "/pages-starter", component: <PagesStarter /> },
   { path: "/my-profile", component: <UserProfile /> },
   { path: "/change-password", component: <ChangePassword /> },
-  { path: "/whatsapp", component: <Whatsapp /> },
   { path: "/chat", component: <Chat /> },
-  // { path: "/playground", component: <EcommerceOrder /> },
   { path: "/", exact: true, component: <Navigate to="/chat" /> },
 ];
 
@@ -64,3 +59,4 @@ const publicRoutes = [
   { path: "/pages-500", component: <Pages500 /> },
 ];
 export { adminOnlyRoutes, authProtectedRoutes, publicRoutes };
+
