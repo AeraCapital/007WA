bcaa78aa668bb3204ca65cdf49c960ac10c37462
unknown <prajwolpakka@gmail.com>
2023-08-14 11:08:32 +0545

    Sidebar highlighting on reload fixed

--- frontend/src/Layouts/VerticalLayout/SidebarContent.jsx
+++ frontend/src/Layouts/VerticalLayout/SidebarContent.jsx
@@ -1,143 +1,13 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-
-//Import Scrollbar
-import SimpleBar from "simplebar-react";
-
-// MetisMenu
-import MetisMenu from "metismenujs";
-
 import { Link } from "react-router-dom";
-
-//i18n
-import { withTranslation } from "react-i18next";
+import SimpleBar from "simplebar-react";
 import withRouter from "../../Components/Common/withRouter";
 
 const SidebarContent = (props) => {
   const ref = useRef();
-  const activateParentDropdown = useCallback((item) => {
-    item.classList.add("active");
-    const parent = item.parentElement;
-    const parent2El = parent.childNodes[1];
 
-    if (parent2El && parent2El.id !== "side-menu") {
-      parent2El.classList.add("mm-show");
-    }
-
-    if (parent) {
-      parent.classList.add("mm-active");
-      const parent2 = parent.parentElement;
-
-      if (parent2) {
-        parent2.classList.add("mm-show"); // ul tag
-
-        const parent3 = parent2.parentElement; // li tag
-
-        if (parent3) {
-          parent3.classList.add("mm-active"); // li
-          parent3.childNodes[0].classList.add("mm-active"); //a
-          const parent4 = parent3.parentElement; // ul
-          if (parent4) {
-            parent4.classList.add("mm-show"); // ul
-            const parent5 = parent4.parentElement;
-            if (parent5) {
-              parent5.classList.add("mm-show"); // li
-              parent5.childNodes[0].classList.add("mm-active"); // a tag
-            }
-          }
-        }
-      }
-      scrollElement(item);
-      return false;
-    }
-    scrollElement(item);
-    return false;
-  }, []);
-
-  const removeActivation = (items) => {
-    for (var i = 0; i < items.length; ++i) {
-      var item = items[i];
-      const parent = items[i].parentElement;
-
-      if (item && item.classList.contains("active")) {
-        item.classList.remove("active");
-      }
-      if (parent) {
-        const parent2El =
-          parent.childNodes && parent.childNodes.lenght && parent.childNodes[1]
-            ? parent.childNodes[1]
-            : null;
-        if (parent2El && parent2El.id !== "side-menu") {
-          parent2El.classList.remove("mm-show");
-        }
-
-        parent.classList.remove("mm-active");
-        const parent2 = parent.parentElement;
-
-        if (parent2) {
-          parent2.classList.remove("mm-show");
-
-          const parent3 = parent2.parentElement;
-          if (parent3) {
-            parent3.classList.remove("mm-active"); // li
-            parent3.childNodes[0].classList.remove("mm-active");
-
-            const parent4 = parent3.parentElement; // ul
-            if (parent4) {
-              parent4.classList.remove("mm-show"); // ul
-              const parent5 = parent4.parentElement;
-              if (parent5) {
-                parent5.classList.remove("mm-show"); // li
-                parent5.childNodes[0].classList.remove("mm-active"); // a tag
-              }
-            }
-          }
-        }
-      }
-    }
-  };
-
-  const activeMenu = useCallback(() => {
-    const pathName = props.router.location.pathname;
-    let matchingMenuItem = null;
-    const ul = document.getElementById("side-menu");
-    const items = ul.getElementsByTagName("a");
-    removeActivation(items);
-
-    for (let i = 0; i < items.length; ++i) {
-      if (pathName === items[i].pathname) {
-        matchingMenuItem = items[i];
-        break;
-      }
-    }
-    if (matchingMenuItem) {
-      activateParentDropdown(matchingMenuItem);
-    }
-  }, [props.router.location.pathname, activateParentDropdown]);
-
-  useEffect(() => {
-    ref.current.recalculate();
-  }, []);
-
-  useEffect(() => {
-    new MetisMenu("#side-menu");
-  }, []);
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-    activeMenu();
-  }, [activeMenu]);
-
-  function scrollElement(item) {
-    if (item) {
-      const currentPosition = item.offsetTop;
-      if (currentPosition > window.innerHeight) {
-        ref.current.getScrollElement().scrollTop = currentPosition - 300;
-      }
-    }
-  }
   const [isAdmin, setIsAdmin] = useState(false);
-
   const { user } = useSelector((state) => ({
     user: state.Login.user,
   }));
@@ -148,48 +18,36 @@ const SidebarContent = (props) => {
       setIsAdmin(obj.user.role === "admin");
     }
   }, [user]);
+
+  const isActivePage = (path) => props.router.location.pathname === path;
+
+  const navLinks = [
+    { path: "/chat", icon: "bx bx-chat", label: "Chat", isAdminOnly: false },
+    { path: "/keywords", icon: "bx bx-file", label: "Keywords", isAdminOnly: true },
+    { path: "/agents", icon: "bx bx-street-view", label: "Agents", isAdminOnly: true },
+  ];
+
   return (
     <React.Fragment>
       <SimpleBar className="h-100" ref={ref}>
         <div id="sidebar-menu">
           <ul className="metismenu list-unstyled" id="side-menu">
-            {/* <li className="menu-title">{props.t("Menu")} </li> */}
-            {/* <li>
-              <Link to="/#">
-                <i className="bx bx-home-circle"></i>
-                <span>{props.t("Dashboard")}</span>
-              </Link>
-            </li> */}
-
-            <li>
-              <Link to="/chat">
-                <i className="bx bx-chat"></i>
-                <span>{props.t("Chat")}</span>
-              </Link>
-            </li>
-
-            {isAdmin && (
-              <li>
-                <Link to="/keywords">
-                  <i className="bx bx-file"></i>
-                  <span>{props.t("Keywords")}</span>
-                </Link>
-              </li>
+            {navLinks.map(
+              ({ path, icon, label, isAdminOnly }) =>
+                (!isAdminOnly || isAdmin) && (
+                  <li key={path}>
+                    <Link to={path}>
+                      <i className={`${icon} ${isActivePage(path) ? "mm-active" : ""}`}></i>
+                      <span className={isActivePage(path) ? "mm-active" : ""}>{label}</span>
+                    </Link>
+                  </li>
+                )
             )}
-
-            {isAdmin && (
-              <li>
-                <Link to="/agents">
-                  <i className="bx bx-street-view"></i>
-                  <span>{props.t("Agents")}</span>
-                </Link>
-              </li>
-            )}
-            {/* <li className="menu-title">{props.t("Apps")}</li> */}
           </ul>
         </div>
       </SimpleBar>
     </React.Fragment>
   );
 };
-export default withRouter(withTranslation()(SidebarContent));
+
+export default withRouter(SidebarContent);
